93632a4707130e8238e074e518d147a155372e0b
Kamil Blazejczyk <kamileczus@gmail.com>
2025-10-17 11:05:31 +0200

    clean up arc constraint class' comments

--- Model/EdgeConstraints/CircleArcEdgeConstraint.cs
+++ Model/EdgeConstraints/CircleArcEdgeConstraint.cs
@@ -47,7 +47,7 @@ public class CircleArcEdgeConstraint : IEdgeConstraint
             var tangentA = g1Continuity.GetContinuityConditions(a, previousEdge, thisEdge)!.Value.TangentVector;
             (center, radius) = CalculateCircle(a, b, tangentA);
             startAngle = GetStartAngle(new Vertex(center.Value.X, center.Value.Y), a);
-            sweepAngle = (float)ToDegrees(GetCentralAngle(a, b, center.Value));
+            sweepAngle = GetSweepAngle(a, b, center.Value);
         }
         else if (a.Continuity is G0Continuity && b.Continuity is G1Continuity g1ContinuityB)
         {
@@ -58,7 +58,7 @@ public class CircleArcEdgeConstraint : IEdgeConstraint
             tangentB.Y *= -1;
             (center, radius) = CalculateCircle(b, a, tangentB);
             startAngle = GetStartAngle(new Vertex(center.Value.X, center.Value.Y), b);
-            sweepAngle = (float)ToDegrees(GetCentralAngle(b, a, center.Value));
+            sweepAngle = GetSweepAngle(b, a, center.Value);
         }
         else
             throw new InvalidOperationException("Two arc vertices have G1 continuity!");
@@ -77,99 +77,72 @@ public class CircleArcEdgeConstraint : IEdgeConstraint
 
     private float GetStartAngle(Vertex center, Vertex point)
     {
-        // Oblicza kąt od osi X do wektora (point - center) za pomocą Atan2
+        // Funkcja oblicza kąt od osi X do wektora (point - center),
+        // czyli początkowy kąt dla funkcji rysującej łuk.
+
         double dx = point.X - center.X;
         double dy = point.Y - center.Y;
 
-        // Math.Atan2(y, x) zwraca kąt w RADIANACH w zakresie (-pi, pi]
-        double radians = Math.Atan2(dy, dx);
-
-        // Konwersja na stopnie
+        double radians = Math.Atan2(dy, dx);
         double degrees = ToDegrees(radians);
-
-        // Zapewnienie, że kąt jest w zakresie [0, 360) (opcjonalne, ale zalecane)
         if (degrees < 0)
             degrees += 360.0;
 
         return (float)degrees;
     }
 
-    public float GetCentralAngle(Vertex a, Vertex b, PointF s)
+    public float GetSweepAngle(Vertex a, Vertex b, PointF s)
     {
-        // 1. Obliczenie wektorów promieniowych
+        // Funkcja oblicza kąt, dla jakiego ma być rysowany łuk okręgu.
+
+        // Obliczenie wektorów promieniowych dla obu wierzchołków.
         double aX = a.X - s.X;
         double aY = a.Y - s.Y;
-
         double bX = b.X - s.X;
         double bY = b.Y - s.Y;
 
-        // 2. Obliczenie kątów dla każdego wektora za pomocą Atan2
-        // Math.Atan2(y, x) zwraca kąt w radianach od osi X w zakresie (-PI, PI]
+        // Obliczenie kątów dla każdego wektora
         double angleA = Math.Atan2(aY, aX);
         double angleB = Math.Atan2(bY, bX);
 
-        // 3. Obliczenie kąta zamiatania (sweep) jako różnicy
+        // Obliczenie kąta jako różnicy i wybór mniejszego z kątów
         double sweepRadians = angleB - angleA;
-
-        // 4. Normalizacja kąta do zakresu (-PI, PI]
-        // (Gdyby np. A = 170 stopni, B = -170 stopni, różnica wyniosłaby -340 stopni.
-        // Chcemy zamiast tego krótszą drogę, czyli +20 stopni).
         if (sweepRadians > Math.PI)
-        {
             sweepRadians -= 2 * Math.PI;
-        }
         else if (sweepRadians <= -Math.PI)
-        {
             sweepRadians += 2 * Math.PI;
-        }
 
-        return (float)sweepRadians;
+        return (float)ToDegrees(sweepRadians);
     }
 
     private (PointF Center, float Radius) CalculateCircle(Vertex a, Vertex b, Vector2 tangentA)
     {
-        // --- Równanie 1: Prosta prostopadła do stycznej w punkcie A ---
-        // Na tej prostej musi leżeć środek S = (xS, yS).
-        // T_x * xS + T_y * yS = C1
+        // Funkcja oblicza środek i promień okręgu na podstawie dwóch wierzchołków a i b
+        // oraz wektora stycznego w wierzchołku a, poprzez rozwiązanie układu równań:
+        // T_x * x_S + T_y * y_S = C1 - równanie prostopadłej do wektora stycznego
+        // (B_x - A_x) * x_S + (B_y - A_y) * y_S = C2 - równanie symetralnej prostej ab
+
+        // Wyznaczenie współczynników w pierwszym równaniu
+        // C1 wyznaczamy podstawiając punkt a do równania prostej
         double Tx = tangentA.X;
         double Ty = tangentA.Y;
         double C1 = Tx * a.X + Ty * a.Y;
 
-        // --- Równanie 2: Symetralna odcinka AB ---
-        // Na tej prostej musi leżeć środek S = (xS, yS).
-        // (Bx - Ax) * xS + (By - Ay) * yS = C2
+        // Wyznaczenie współczynników w drugim równaniu
+        // Wzór na C2 wynika z warunku na równą odległość między aS i Sb
         double DiffX = b.X - a.X;
         double DiffY = b.Y - a.Y;
         double C2 = 0.5 * (b.X * b.X - a.X * a.X + b.Y * b.Y - a.Y * a.Y);
 
-        // Rozwiązanie układu równań liniowych:
-        // Tx * xS + Ty * yS = C1
-        // DiffX * xS + DiffY * yS = C2
-
-        // Obliczanie wyznacznika głównego (D)
+        // Rozwiązanie układu równań metodą Cramera
         double D = Tx * DiffY - Ty * DiffX;
-
-        // --- Sprawdzenie, czy linie są równoległe (D = 0) ---
-        //if (Math.Abs(D) < 1e-9) // Używamy tolerancji dla porównań zmiennoprzecinkowych
-        //{
-        //    // Oznacza to, że wektor styczny T_A jest współliniowy z wektorem AB.
-        //    // Jedynym rozwiązaniem jest łuk o nieskończonym promieniu (odcinek AB).
-        //    return new ArcParameters { IsValid = false, Error = "T_A jest współliniowy z A->B (nieskończony promień/prosta)." };
-        //}
-
-        // --- Obliczanie współrzędnych środka S (z użyciem reguły Cramera) ---
-
-        // Wyznacznik Dx
         double Dx = C1 * DiffY - Ty * C2;
-        // Wyznacznik Dy
         double Dy = Tx * C2 - C1 * DiffX;
-
         double Sx = Dx / D;
         double Sy = Dy / D;
 
+        // Wyznaczenie środka i promienia
         PointF S = new PointF((float)Sx, (float)Sy);
-
-        // --- Obliczanie promienia R ---
         float radius = (float)Math.Sqrt((a.X - Sx) * (a.X - Sx) + (a.Y - Sy) * (a.Y - Sy));
 
         return (S, radius);
