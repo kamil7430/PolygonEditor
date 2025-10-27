1a2872704fe5560ddc3c0ad692ac610cb7a57780
Kamil Blazejczyk <kamileczus@gmail.com>
2025-10-27 15:21:45 +0100

    add JsonDerivedType to interface

--- Model/Dtos/VertexContinuities/IVertexContinuityDto.cs
+++ Model/Dtos/VertexContinuities/IVertexContinuityDto.cs
@@ -1,7 +1,11 @@
 ﻿using PolygonEditor.Model.VertexContinuities;
+using System.Text.Json.Serialization;
 
 namespace PolygonEditor.Model.Dtos.VertexContinuities;
 
+[JsonDerivedType(typeof(G0ContinuityDto), 0)]
+[JsonDerivedType(typeof(G1ContinuityDto), 1)]
+[JsonDerivedType(typeof(C1ContinuityDto), 2)]
 public interface IVertexContinuityDto
 {
     IVertexContinuity GetContinuity(Polygon polygon);
